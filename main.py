cd447b43892b0354fd7ffcadbcf868c7e3ec8667
Julius Hermanto <juliushmto@gmail.com>
2024-09-28 10:20:39 +0700

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from src.loanDefault import logger
+from src.loanDefault.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Welcome to my custom log for this deployment")
+STAGE_NAME = "Data ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
