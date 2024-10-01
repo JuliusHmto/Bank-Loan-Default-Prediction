c622837db72d3fe7a9d9a3cbab3c407253d8592c
Julius Hermanto <juliushmto@gmail.com>
2024-10-01 21:02:26 +0700

    data validation

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from src.loanDefault import logger
 from src.loanDefault.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.loanDefault.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
-STAGE_NAME = "Data ingestion stage"
+STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
